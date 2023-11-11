5b15073cd5464210a1b560522f5c3ac1d59e6d43
Joabriel <Quijanojoaquingabriel@gmail.com>
2023-11-11 03:11:57 -0300

    Triangulación de variables (XYe YEquis)!

--- FullCourse.java
+++ FullCourse.java
@@ -2,7 +2,14 @@ class FullCourse{
     public static void main(String[]args){
         String x = "Equis";
         String y = "Ye";
+        String temp;
 
-        System.out.print(x+" "+y);
+        temp=x;
+        x=y;
+        y=temp;
+        
+        System.out.print("X"+x+" "+"Y"+y);
+
+        //XYe //YEquis.
     }
 }
