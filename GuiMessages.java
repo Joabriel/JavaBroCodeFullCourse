3679d124fb29be49f88d6af0d9a285f0e0698679
Joabriel <Quijanojoaquingabriel@gmail.com>
2023-11-13 21:35:26 -0300

    incluido JOptionPane con entero

--- GuiMessages.java
+++ GuiMessages.java
@@ -2,8 +2,12 @@ import javax.swing.JOptionPane; //Librería para interfaces GUI.
 class FullCourse{
     public static void main(String[]args){
 
-        String name = JOptionPane.showInputDialog("Ingrese su nombre: ");
-        JOptionPane.showMessageDialog(null, "Hola: "+name+"!"); //Es importante poner el "Null" aunque todavía no se por que.
+        String nombre = JOptionPane.showInputDialog("Nombre: ");
+        int edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su edad:")); /*Se debe transformar
+         cualquier tipo de dato porque solo acepta Strings aparentemente.*/
+        JOptionPane.showMessageDialog(null,"NOMBRE: "+nombre);
+        JOptionPane.showMessageDialog(null,"EDAD: "+edad);
+        
 
     }
 }
