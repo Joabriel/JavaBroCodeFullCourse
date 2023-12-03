fb745a79ad9a71b64e1fd28407fde19feca5c39d
Joabriel <Quijanojoaquingabriel@gmail.com>
2023-12-03 18:58:47 -0300

    Clase Math: programa que saca la hipotenusa.

--- FullCourse.java
+++ FullCourse.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
 class FullCourse{
     public static void main(String[]args){
-        String x = "Equis";
-        String y = "Ye";
+        Scanner reader = new Scanner(System.in);
+        double x;
+        double y;
+        double z;
 
-        System.out.print(x+" "+y);
+        System.out.print("Enter side x: ");
+        x = reader.nextDouble();
+        System.out.print("Enter side y: ");
+        y = reader.nextDouble();
+
+        z = Math.sqrt((x*x)+(y*y));
+
+        System.out.print("La Hipotenusa es: "+z);
+
+        reader.close();
     }
 }
